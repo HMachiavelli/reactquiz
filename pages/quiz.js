7e122df6334d85a84918083bad3025f3b881fb0c
Henrique Machiavelli <hsmachiavelli@gmail.com>
2021-02-02 21:36:06 -0300

    inclusão de alternativas na tela de quiz

--- pages/quiz.js
+++ pages/quiz.js
@@ -9,39 +9,73 @@ import QuizBackground from '../src/components/QuizBackground';
 import QuizContainer from '../src/components/QuizContainer';
 import QuizLogo from '../src/components/QuizLogo';
 
+function QuestionWidget({ question, totalQuestions, questionIndex }) {
+  const questionId = `question__${questionIndex}`;
+
+  return (
+    <Widget>
+      <Widget.Header>
+        {/* <button type="button">Back</button> */}
+        <h3>
+          {`Pergunta ${questionIndex + 1} de ${totalQuestions}`}
+        </h3>
+      </Widget.Header>
+
+      <img
+        alt={question.title}
+        style={{
+          width: '100%',
+          height: '150px',
+          objectFit: 'cover',
+        }}
+        src={question.image}
+      />
+
+      <Widget.Content>
+        <h2 style={{ marginBottom: '20px' }}>{question.title}</h2>
+
+        <form>
+          {question.alternatives.map((item, index) => {
+            return (
+              <Widget.Topic
+                as="label"
+                htmlFor={index}
+              >
+                <input
+                  type="radio"
+                  name={questionId}
+                  value={index}
+                  id={index}
+                  style={{ marginRight: '10px' }}
+                />
+                {item}
+              </Widget.Topic>
+            );
+          })}
+        </form>
+
+        <Button type="submit">CONFIRMAR</Button>
+      </Widget.Content>
+    </Widget>
+  );
+}
+
 export default function Quiz() {
   const router = useRouter();
   const { name } = router.query;
+
+  const question = db.questions[0];
 
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
         <QuizLogo />
-        <Widget>
-          <Widget.Header>
-            <button type="button">Back</button>
-            <h3>
-              Pergunta 1 de
-              {` ${db.questions.length}`}
-            </h3>
-          </Widget.Header>
 
-          <img
-            alt="Descrição"
-            style={{
-              width: '100%',
-              height: '150px',
-              objectFit: 'cover',
-            }}
-            src="http://placehold.jp/400x150.png"
-          />
-
-          <Widget.Content>
-            <h2>Pergunta</h2>
-
-            <Button type="submit">CONFIRMAR</Button>
-          </Widget.Content>
-        </Widget>
+        <QuestionWidget
+          question={question}
+          totalQuestions={db.questions.length}
+          questionIndex={0}
+        />
       </QuizContainer>
     </QuizBackground>
   );
